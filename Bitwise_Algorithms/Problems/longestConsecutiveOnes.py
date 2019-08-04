c6a3b031f76470ff935d4e2fd1b83d24d0db7422
Padma <vikasch.1994@gmail.com>
2019-08-04 17:33:38 +0530

    Longest subsequence of ones in bin(N)

---
+++ Bitwise_Algorithms/Problems/longestConsecutiveOnes.py
@@ -0,0 +1,24 @@
+{
+#Initial Template for Python 3
+import math
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        print(maxConsecutiveOnes(n))
+        T-=1
+if __name__=="__main__":
+    main()
+}
+''' This is a function problem.You only need to complete the function given below '''
+#User function Template for python3
+##Complete this function
+def maxConsecutiveOnes(x):
+    ##Your code here
+    bin_rep = bin(x)[2:]
+    str1s = bin_rep.split('0')
+    return (max([len(u) for u in str1s]))
