189f05fea1657e62ec0382ffb23be25a4fad796b
ADMIN <vikasch.1994@gmail.com>
2019-09-22 16:01:01 +0530

    Repeating elements using O(n) Auxiliary space - no TLE

---
+++ Searching/Problems/find2RepeatingElements_V3.0.py
@@ -0,0 +1,29 @@
+{
+#Initial Template for Python 3
+import math
+    
+def main():
+        T=int(input())
+        while(T>0):
+            
+            N=int(input())
+            A=[int(x) for x in input().strip().split()]
+            
+            twoRepeated(A,N)
+            print()
+            
+            T-=1
+if __name__ == "__main__":
+    main()
+}
+''' This is a function problem.You only need to complete the function given below '''
+#User function Template for python3
+##Complete this code
+def twoRepeated(arr,n):
+    #Your code here
+    count = [0] * (n+2)
+    for i in range(0, n+2) : 
+        if(count[arr[i]] == 1) : 
+            print(arr[i], end = " ") 
+        else : 
+            count[arr[i]] = count[arr[i]] + 1
