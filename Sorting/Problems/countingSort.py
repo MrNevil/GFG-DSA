0f928b9c9c94f0611b96ff65dc9932cc1cf6efad
Vikas <vikasch.1994@gmail.com>
2019-10-06 09:59:50 +0530

    sort an array using counting sort - one line code using sorted()

---
+++ Sorting/Problems/countingSort.py
@@ -0,0 +1,33 @@
+{
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+#Contributed by : Nagendra Jha
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+    
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        s=str(input())
+        countingSort(s,n)
+        print("")
+}
+''' This is a function problem.You only need to complete the function given below '''
+#User function Template for python3
+'''
+Function Arguments :
+		@param  : s(given string), n (size of string)
+		@return : None, print the sorted string
+'''
+def countingSort(s,n):
+    # code here
+    s = sorted(s)
+    print ("".join(s),end="")
