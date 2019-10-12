8cb3a975477533ce56542c434368d6cf3d36541b
Vikas <vikasch.1994@gmail.com>
2019-10-12 23:40:21 +0530

    transpose of matrix

---
+++ Matrix/Problems/TransposeOfMatrix.py
@@ -0,0 +1,41 @@
+{
+#Initial Template for Python 3
+			
+def main():
+    T=int(input())
+    while(T>0):
+        n=int(input())
+        mat=[[0 for j in range(n)] for i in range(n)]
+        line1=[int(x) for x in input().strip().split()]
+        
+       
+        k=0
+        for i in range(n):
+            for j in range (n):
+                mat[i][j]=line1[k]
+                k+=1
+       
+        k=0
+       
+        transpose(mat,n)
+        for i in range(n):
+            for j in range(n):
+                print(mat[i][j],end=" ")
+        print()
+            
+        
+        T-=1
+if __name__=="__main__":
+    main()
+}
+''' This is a function problem.You only need to complete the function given below '''
+#User function Template for python3
+##Complete this function
+def transpose(mat,n):
+    ##Your code here
+    tmp = 0
+    for i in range(n):
+        for j in range(i+1,n):
+            tmp = mat[i][j]
+            mat[i][j] = mat[j][i]
+            mat[j][i] = tmp
