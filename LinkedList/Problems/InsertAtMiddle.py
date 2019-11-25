b00a54a328b12cd20f465edabb873e14e58d4481
absognety <vikasch.1994@gmail.com>
2019-11-25 14:46:38 +0530

    insert at the middle of linked list

---
+++ LinkedList/Problems/InsertAtMiddle.py
@@ -0,0 +1,89 @@
+{
+#Initial Template for Python 3
+#Contributed by : Nagendra Jha
+import atexit
+import io
+import sys
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+    
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+        
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        curr_node = self.head
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+        curr_node.next = new_node
+    
+    # prints the elements of linked list starting with head
+    def printList(self):
+        if self.head is None:
+            print(' ')
+            return
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.data,end=" ")
+            curr_node=curr_node.next
+        print(' ')
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList()
+        nodes = list(map(int, input().strip().split()))
+        for x in nodes:
+            a.append(x)
+        mid_elem = int(input())
+        insertInMid(a.head, Node(mid_elem) )
+        a.printList()
+}
+''' This is a function problem.You only need to complete the function given below '''
+#User function Template for python3
+'''
+    Your task is to insert a new node in 
+	the middle of the linked list with
+	the given value.
+	
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+	
+	Function Arguments: head (head of linked list), node (node to be inserted in middle)
+	Return Type: None, just insert the new node at mid.
+'''
+def insertInMid(head,node):
+    #code here
+    newNode = node
+    if head == None:
+        head = node
+        return
+    slow=head
+    fast = head.next
+    while (fast != None and fast.next != None):
+        slow = slow.next
+        fast = fast.next.next
+    newNode.next = slow.next
+    slow.next = newNode
