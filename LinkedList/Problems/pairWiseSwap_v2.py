5f73afc8afb0fe7afb54efb3c73499fa71ce53bc
absognety <vikasch.1994@gmail.com>
2019-11-30 19:17:03 +0530

    pair wise swap of elements in a linked list - version 2.0

---
+++ LinkedList/Problems/pairWiseSwap_v2.py
@@ -0,0 +1,80 @@
+{
+#Initial Template for Python 3
+#Contributed by : Nagendra Jha
+import atexit
+import io
+import sys
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        curr_node = self.head
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+        curr_node.next = new_node
+# prints the elements of linked list starting with head
+def printList(head):
+    if head is None:
+        print(' ')
+        return
+    curr_node = head
+    while curr_node:
+        print(curr_node.data,end=" ")
+        curr_node=curr_node.next
+    print(' ')
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+        resultant_head=(pairwiseSwap(a.head))
+        printList(resultant_head)
+}
+''' This is a function problem.You only need to complete the function given below '''
+#User function Template for python3
+'''
+	Your task is to pairwise nodes of 
+	the given linked list.
+	
+	Function Arguments: head of the original list.
+	Return Type: reference to head of the given linked list.
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+'''
+# swap pairwise nodes of the linked list and return the head
+def pairwiseSwap(head):
+    #code here
+    temp = None
+    curr_node = head
+    while curr_node != None and curr_node.next != None:
+        temp = curr_node.data
+        curr_node.data = curr_node.next.data
+        curr_node.next.data = temp
+        curr_node = curr_node.next.next
+    return head
