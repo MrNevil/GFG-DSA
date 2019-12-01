36fa6f0db926ebdfbeb5c5d426ec6139cf1336f8
absognety <vikasch.1994@gmail.com>
2019-12-01 16:01:00 +0530

    is linked list a palindrome?

---
+++ LinkedList/Problems/isPalindrome.py
@@ -0,0 +1,81 @@
+{
+#Initial Template for Python 3
+#Contributed by : Nagendra Jha
+import atexit
+import io
+import sys
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        curr_node = self.head
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+        curr_node.next = new_node
+# returns the size of linked list
+def getSize(head):
+    count = 0
+    curr_node = head
+    while curr_node:
+        count +=1
+        curr_node = curr_node.next
+    return count
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+        if isPalindrome(a.head):
+            print(1)
+        else:
+            print(0)
+}
+''' This is a function problem.You only need to complete the function given below '''
+#User function Template for python3
+'''
+	Your task is to check if given linkedlist
+	is a pallindrome or not.
+	
+	Function Arguments: head (reference to head of the linked list)
+	Return Type: boolean , no need to print just return True or False.
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+	Contributed By: Nagendra Jha
+'''
+def isPalindrome(head):
+    #code here
+    elements = []
+    curr_node = head
+    while curr_node != None:
+        elements.append(curr_node.data)
+        curr_node = curr_node.next
+    if elements == elements[::-1]:
+        return True
+    else:
+        return False
