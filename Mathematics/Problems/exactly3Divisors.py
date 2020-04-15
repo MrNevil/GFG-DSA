e45829f1bea8eca6d8d1cba4c201634338bbcc0c
absognety <vikasch.1994@gmail.com>
2020-04-15 14:13:16 +0530

    Add problem statement with description

--- Mathematics/Problems/exactly3Divisors.py
+++ Mathematics/Problems/exactly3Divisors.py
@@ -3,6 +3,46 @@
   
 # program for finding  
 # no. of divisors 
+"""
+Given a positive integer value N. The task is to find how many numbers less than 
+or equal to N have numbers of divisors exactly equal to 3.
+
+Input:
+The first line contains integer T, denoting number of test cases. Then T test 
+cases follow. The only line of each test case contains an integer N.
+
+Output:
+For each testcase, in a new line, print the answer of each test case.
+
+Your Task:
+This is a function problem. You only need to complete the function 
+exactly3Divisors() that takes N as parameter and returns count of numbers less 
+than or equal to N with exactly 3 divisors.
+
+Constraints :
+1 <= T <= 10^5
+1 <= N <= 10^9
+
+Example:
+Input :
+3
+6
+10
+30
+Output :
+1
+2
+3
+
+Explanation:
+Testcase 1: There is only one number 4 which has exactly three divisors 1, 2 and 
+4.
+Testcase 2: 4 and 9 are the only two numbers less than or equal to 10 that have 
+exactly three divisors.
+Testcase 3: 4, 9, 25 are the only numbers less than or equal to 30 that have 
+exactly three divisors.
+ 
+"""
 
 def exactly3Divisors(N):
     def divCount(n): 
