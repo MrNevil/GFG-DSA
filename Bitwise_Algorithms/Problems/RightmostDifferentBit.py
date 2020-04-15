478a1ff7bad9bd0c12718a2f431b87f21fb0d7fe
absognety <vikasch.1994@gmail.com>
2020-04-15 19:37:42 +0530

    rightmost different bit in binary reps of 2 numbers

--- Bitwise_Algorithms/Problems/RightmostDifferentBit.py
+++ Bitwise_Algorithms/Problems/RightmostDifferentBit.py
@@ -1,3 +1,43 @@
+"""
+Given two numbers M and N. The task is to find the position of rightmost 
+different bit in binary representation of numbers.
+
+Input Format:
+The input line contains T, denoting the number of testcases. Each testcase 
+follows. First line of each testcase contains two space separated integers M and 
+N.
+
+Output Format:
+For each testcase in new line, print the position of rightmost different bit in 
+binary representation of numbers. If both M and N are same then print -1 in this 
+case.
+
+User Task:
+The task is to complete the function posOfRightMostDiffBit() which takes two 
+arguments m and n and returns the position of first different bits in m and n.
+
+Constraints:
+1 <= T <= 100
+1 <= M <= 10^3
+1 <= N <= 10^3
+
+Example:
+Input:
+2
+11 9
+52 4
+
+Output:
+2
+5
+
+Explanation:
+Tescase 1: Binary representaion of the given numbers are: 1011 and 1001, 2nd bit 
+from right is different.
+Testcase 2: Binary representation of the given numbers are:  ‭110100‬ and 0100, 
+5th bit fron right is different.
+
+"""
 {
 #Initial Template for Python 3
 import math
